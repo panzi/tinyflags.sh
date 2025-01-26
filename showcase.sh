c99a550b31805b7ede569ff7d99e2f6cc488f66f
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-01-26 01:42:04 +0100

    add showcase.sh

---
+++ showcase.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+cat <<EOF
+
+         $(./tinyflags.sh at)                 $(./tinyflags.sh bd)           $(./tinyflags.sh be)           $(./tinyflags.sh bj)               $(./tinyflags.sh ch)
+     Austria (at)      Bangladesh (bd)  Belgium (be)   Benin (bj)     Switzerland (ch)
+
+         $(./tinyflags.sh ci)                 $(./tinyflags.sh en)            $(./tinyflags.sh fr)           $(./tinyflags.sh gn)               $(./tinyflags.sh gw)
+   Côte d'Ivoire (ci)    England (en)   France (fr)    Guinea (gn)   Guinea-Bissau (gw)
+
+         $(./tinyflags.sh ic)                 $(./tinyflags.sh id)            $(./tinyflags.sh ir)           $(./tinyflags.sh it)               $(./tinyflags.sh jp)
+  Canary Islands (ic)   Indonesia (id)  Ireland (ir)   Italy (it)        Japan (jp)
+
+         $(./tinyflags.sh lv)                 $(./tinyflags.sh ma)            $(./tinyflags.sh mc)            $(./tinyflags.sh mn)               $(./tinyflags.sh mg)
+     Latvia (lb)         Morroco (ma)   Monaco (mc)   Mongolia (mn)   Madagascar (mg)
+
+         $(./tinyflags.sh ml)                 $(./tinyflags.sh ng)            $(./tinyflags.sh pe)           $(./tinyflags.sh pl)              $(./tinyflags.sh pw)
+      Mali (ml)          Nigeria (ng)    Peru (pe)     Poland (pl)      Palau (pw)
+
+         $(./tinyflags.sh ro)                 $(./tinyflags.sh sn)            $(./tinyflags.sh so)           $(./tinyflags.sh td)              $(./tinyflags.sh tr)
+      Romania (ro)       Senegal (sn)   Somalia (so)   Chad (td)        Turkie (tr)
+
+         $(./tinyflags.sh ua)                 $(./tinyflags.sh vn)
+     Ukraine (ua)        Vietnam (vn)
+
+         $(./tinyflags.sh refugee)                 $(./tinyflags.sh red-cross)            $(./tinyflags.sh jolly-roger)           $(./tinyflags.sh finish)
+       Refugee            Red Cross     Jolly Roger     Finish
+
+EOF
